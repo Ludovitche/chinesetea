134a7ce91fe3114ec80271320bc04cdc415f60ad
Ludovitche <Ludovitche@users.noreply.github.com>
2019-07-21 14:35:27 +0700

    replace deprecated function - 3

--- server.js
+++ server.js
@@ -2,18 +2,22 @@ const express = require('express');
 const app = express();
 var pg = require('pg');
 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+})  
+
 app.get('/', (req, res) => {
   res.send('Hello, Dokku!');
-});
+  pool.connect(function(err, client, done) {
+    client.query('SELECT * FROM format', function(err, result) {
+      done();
+      if (err) {
+        console.log(err);
+        res.status(400).send(err);
+      }
+      res.status(200).send(result.rows);
+  });
+  });
 
 // Port 5000 is the default Dokku application port
 app.listen(5000, () => console.log('Listening on port 5000'));
-
-var pool = new pg.Pool()
-pool.connect(process.env.DATABASE_URL, function(err, client, done) {
-  client.query('SELECT * FROM format', function(err, result) {
-     if(err) return console.error(err);
-     console.log(result.rows);
-     done();
-  });
-});
