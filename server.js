1ba6a1e95f738614abde010e29e9d34cf2becad6
Ludovitche <Ludovitche@users.noreply.github.com>
2019-07-21 13:58:14 +0700

    testing connection to database on dokku

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
+var pg = require('pg');
 
 app.get('/', (req, res) => {
-  res.send('Hello, Dokku!' + process.env.DATABASE_URL);
+  res.send('Hello, Dokku!');
 });
 
 // Port 5000 is the default Dokku application port
 app.listen(5000, () => console.log('Listening on port 5000'));
+
+var pg=require("pg");
+
+pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  client.query('SELECT * FROM format', function(err, result) {
+     done();
+     if(err) return console.error(err);
+     console.log(result.rows);
+  });
+});
