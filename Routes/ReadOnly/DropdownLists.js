e64191acf4dace84dd0e7a6eb4fb58f59a68f55a
Ludovitche <Ludovitche@users.noreply.github.com>
2019-08-01 00:15:03 +0700

    implement all queries for dropdown lists

--- Routes/ReadOnly/DropdownLists.js
+++ Routes/ReadOnly/DropdownLists.js
@@ -2,8 +2,28 @@
 
 const db = require('../../DB')
 
-const getCurrentRoleListWithId = db.rawDataSimpleQuery('SELECT currentroleid, name FROM CurrentRole')
+const getCurrentRoleListWithId = db.rawDataSimpleQuery('SELECT CurrentRoleId, Name FROM CurrentRole')
+const getTypeListWithId = db.rawDataSimpleQuery('SELECT TypeId, Name FROM Type')
+const getLocationListWithId = db.rawDataSimpleQuery('SELECT LocationId, Name FROM Location')
+const getShopListWithId = db.rawDataSimpleQuery('SELECT ShopId, Name, Url FROM Shop')
+const getFormatListWithId = db.rawDataSimpleQuery('SELECT FormatId, Name FROM Format')
+const getCurrencyListWithId = db.rawDataSimpleQuery('SELECT CurrencyId, Code, Name FROM Currency')
+const getCountryListWithId = db.rawDataSimpleQuery('SELECT CountryId, Name FROM Country')
+
+const getSubTypeListWithId = (req, res) => db.rawDataQuery('SELECT SubTypeId, Name FROM SubType WHERE TypeId=$1', req.params)
+const getAreaListWithId = (req, res) => db.rawDataQuery('SELECT AreaId, Name FROM Area WHERE CountryId=$1', req.params)
+
 
 module.exports = {
-	getCurrentRoleListWithId: getCurrentRoleListWithId
+	getTypeListWithId: getTypeListWithId,
+	getSubTypeListWithId: getSubTypeListWithId,
+
+	getCountryListWithId: getCountryListWithId,
+	getAreaListWithId: getAreaListWithId,
+
+	getFormatListWithId: getFormatListWithId,
+	getShopListWithId: getShopListWithId,
+	getCurrencyListWithId: getCurrencyListWithId,
+	getLocationListWithId: getLocationListWithId,
+	getCurrentRoleListWithId: getCurrentRoleListWithId,
 }
--- server.js
+++ server.js
@@ -3,9 +3,20 @@
 const express = require('express');
 const app = express();
 
-const {getCurrentRoleListWithId} = require('./Routes/ReadOnly/DropdownLists');
+const dl = require('./Routes/ReadOnly/DropdownLists');
 
-app.get("/currentRoles", getCurrentRoleListWithId);
+app.get("/countries", dl.getCountryListWithId);
+app.get("/areas/:countryId", dl.getAreaListWithId);
+
+app.get("/types", dl.getTypeListWithId);
+app.get("/subTypes/:typeId", dl.getSubTypeListWithId);
+
+app.get("/formats", dl.getFormatListWithId);
+app.get("/shops", dl.getShopListWithId);
+app.get("/currencies", dl.getCurrencyListWithId);
+app.get("/locations", dl.getLocationListWithId);
+app.get("/currentRoles", dl.getCurrentRoleListWithId);
+
 
 // This needs to be last
 app.use(function(req, res) {
