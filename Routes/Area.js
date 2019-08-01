d8b99849c7ccadc455c35f704ec79beb9a27b1cb
Ludovitche <ludovitche@gmail.com>
2019-08-01 16:35:01 +0700

    solve name conflict in sql request'

--- Routes/Area.js
+++ Routes/Area.js
@@ -2,7 +2,7 @@
 
 const db = require('../DB')
 
-const getAllAreasWithCountryName = db.rawDataSimpleQuery('SELECT A.AreaId, A.Name, C.CountryId, C.Name FROM Area A join Country C on A.CountryId=C.CountryId')
+const getAllAreasWithCountryName = db.rawDataSimpleQuery('SELECT A.AreaId, A.Name, C.CountryId, C.Name as CountryName FROM Area A join Country C on A.CountryId=C.CountryId')
 const getAreaListByCountry = db.rawDataQuery('SELECT AreaId, Name FROM Area WHERE CountryId=$1',['countryId'])
 
 module.exports = {
--- Routes/SubType.js
+++ Routes/SubType.js
@@ -2,7 +2,7 @@
 
 const db = require('../DB')
 
-const getAllSubTypesWithTypeName = db.rawDataSimpleQuery('SELECT S.SubTypeId, S.Name, T.TypeId, T.Name FROM SubType S join Type T on S.typeId=T.TypeId')
+const getAllSubTypesWithTypeName = db.rawDataSimpleQuery('SELECT S.SubTypeId, S.Name, T.TypeId, T.Name as TypeName FROM SubType S join Type T on S.typeId=T.TypeId')
 const getSubTypeListByType = db.rawDataQuery('SELECT SubTypeId, Name FROM SubType WHERE TypeId=$1',['typeId'])
 
 module.exports = {
