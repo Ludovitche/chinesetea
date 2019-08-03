d1b865a6a4b78f3de80c5f511a73921a7b250d43
Ludovitche <Ludovitche@users.noreply.github.com>
2019-08-03 18:13:48 +0700

    experiment 1 to learn map reduce - corrected 7

--- Routes/Order.js
+++ Routes/Order.js
@@ -4,29 +4,28 @@ const db = require('../DB')
 
 const getAllOrdersWithTeaList = (req, res) => {
 	return db.simpleQuery(`
-	SELECT T.Name as TeaName, S.Name as ShopName, O.OrderId, O.OrderNumber, O.TrackingNumber, O.Date, 
+	SELECT T.TeaId, T.Name as TeaName, S.Name as ShopName, O.OrderId, O.OrderNumber, O.TrackingNumber, O.Date, 
 	O.TotalAmountInBaht, O.ShippingCostInBaht, O.TotalAmountInUsdCents, ShippingCostInUsdCents 
 	
 	FROM "order" O join Shop S on S.ShopId=O.ShopId left join OrderTea OT on O.OrderId=OT.OrderId 
 	left join Tea T on OT.TeaId=T.TeaId`)
 
-		.then(data => data.rows.reduce((orderList, row) => {
-			let orderId = row.orderid - 1
-			let { teaname, ...orderData } = row
-			if (!orderList[orderId]) {
-				orderList[orderId] = ({
-					...orderData,
-					teaList: teaname ? [teaname] : []
-				})
+	.then(data => data.rows.reduce((orderList, row) => {
+		let orderId = row.orderid - 1
+		let { teaname, teaid, ...orderData } = row
+		if (!orderList[orderId]) {
+			orderList[orderId] = ({
+				...orderData,
+				teaList: teaid ? [{teaId: teaid, teaName: teaname}] : []
+			})
+		}
+		else {
+			if (teaid) {
+				orderList[orderId].teaList.push({teaId: teaid, teaName: teaname})
 			}
-			else {
-				if (teaname) {
-					orderList[orderId].teaList.push(teaname)
-				}
-			}
-			return orderList;
-		}, [])
-	)
+		}
+		return orderList;
+	}, []))
 	.then(data => res.status(200).send(data))
 	.catch(e => {
 		console.log(e.stack)
