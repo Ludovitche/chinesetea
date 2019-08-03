32d6bc895df568b00bd1d377b7fc28b8990e6e93
Ludovitche <Ludovitche@users.noreply.github.com>
2019-08-03 23:33:16 +0700

    correct sql query

--- Routes/SubType.js
+++ Routes/SubType.js
@@ -3,8 +3,8 @@
 const db = require('../DB')
 
 const getAllSubTypesWithTypeNameCheckFK = db.rawDataSimpleQuery(`
-SELECT DISTINCT S.SubTypeId, S.Name, T.TypeId, T.Name as TypeName, T.TeaId is null as CanDelete
-FROM SubType S join Type T on S.typeId=T.TypeId left join Tea T on S.SubTypeId=T.SubTypeId
+SELECT DISTINCT S.SubTypeId, S.Name, TY.TypeId, TY.Name as TypeName, T.TeaId is null as CanDelete
+FROM SubType S join Type TY on S.typeId=TY.TypeId left join Tea T on S.SubTypeId=T.SubTypeId
 `)
 const getSubTypeListByType = db.rawDataQuery(`
 SELECT SubTypeId, Name FROM SubType 
