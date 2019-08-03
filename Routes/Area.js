61b6e5348b4a9531b601ad2c5a3165aeda12e3ba
Ludovitche <Ludovitche@users.noreply.github.com>
2019-08-03 11:20:43 +0700

    Make sql queries easier to rtead using back-tick

--- Routes/Area.js
+++ Routes/Area.js
@@ -2,8 +2,15 @@
 
 const db = require('../DB')
 
-const getAllAreasWithCountryName = db.rawDataSimpleQuery('SELECT A.AreaId, A.Name, C.CountryId, C.Name as CountryName FROM Area A join Country C on A.CountryId=C.CountryId')
-const getAreaListByCountry = db.rawDataQuery('SELECT AreaId, Name FROM Area WHERE CountryId=$1',['countryId'])
+const getAllAreasWithCountryName = db.rawDataSimpleQuery(`
+SELECT A.AreaId, A.Name, C.CountryId, C.Name as CountryName 
+FROM Area A join Country C on A.CountryId=C.CountryId
+`)
+const getAreaListByCountry = db.rawDataQuery(`
+SELECT AreaId, Name 
+FROM Area 
+WHERE CountryId=$1`
+,['countryId'])
 
 module.exports = {
 	getAllAreasWithCountryName: getAllAreasWithCountryName,
--- Routes/SubType.js
+++ Routes/SubType.js
@@ -2,8 +2,13 @@
 
 const db = require('../DB')
 
-const getAllSubTypesWithTypeName = db.rawDataSimpleQuery('SELECT S.SubTypeId, S.Name, T.TypeId, T.Name as TypeName FROM SubType S join Type T on S.typeId=T.TypeId')
-const getSubTypeListByType = db.rawDataQuery('SELECT SubTypeId, Name FROM SubType WHERE TypeId=$1',['typeId'])
+const getAllSubTypesWithTypeName = db.rawDataSimpleQuery(`
+SELECT S.SubTypeId, S.Name, T.TypeId, T.Name as TypeName 
+FROM SubType S join Type T on S.typeId=T.TypeId`)
+const getSubTypeListByType = db.rawDataQuery(`
+SELECT SubTypeId, Name FROM SubType 
+WHERE TypeId=$1`
+,['typeId'])
 
 module.exports = {
 	getAllSubTypesWithTypeName: getAllSubTypesWithTypeName,
