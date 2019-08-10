e6f4f3b14089a810d6b89459e6bbc44dd3ee0875
Ludovitche <Ludovitche@users.noreply.github.com>
2019-08-10 18:29:50 +0700

    complete dropdown list GET

--- routes/teaproperties.js
+++ routes/teaproperties.js
@@ -1,22 +1,31 @@
 "use strict";
 
 const db = require("../db");
-const queries = require("../query");
 
-const SQL_QUERY_GET_AREAS = `
-SELECT AreaId, Name, CountryId
-FROM Area`;
+const SQL_QUERY_GET_FORMATS = `SELECT FormatId, Name FROM Format`;
+const SQL_QUERY_GET_ROLES = `SELECT CurrentRoleId, Name FROM CurrentRole`;
+const SQL_QUERY_GET_LOCATIONS = `SELECT LocationId, Name FROM Location`;
 
-const getAllFormats = queries.getQueryNoParams(
-  "SELECT FormatId, Name FROM Format"
-);
-const getAllCurrentRoles = queries.getQueryNoParams(
-  "SELECT CurrentRoleId, Name FROM CurrentRole"
-);
-const getAllLocations = queries.getQueryNoParams(
-  "SELECT LocationId, Name FROM Location"
-);
+const getAllTeaDropdownLists = (req, res) => {
+  let result = {};
+  Promise.all(
+    db
+      .simpleQuery(SQL_QUERY_GET_FORMATS)
+      .then(data => (result.formats = data.rows)),
+    db
+      .simpleQuery(SQL_QUERY_GET_ROLES)
+      .then(data => (result.currentRoles = data.rows)),
+    db
+      .simpleQuery(SQL_QUERY_GET_LOCATIONS)
+      .then(data => (result.locations = data.rows))
+  )
+    .then(data => res.status(200).send(result))
+    .catch(e => {
+      console.log(e.stack);
+      res.status(500).send(e);
+    });
+};
 
 module.exports = {
-  getAllTeaDropdownLists: getAllFormats
+  getAllTeaDropdownLists: getAllTeaDropdownLists
 };
