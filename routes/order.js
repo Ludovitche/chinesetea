549347863a4a362f50b41208225f5f1ff1d6bdc0
Ludovitche <Ludovitche@users.noreply.github.com>
2019-08-10 17:22:32 +0700

    more (useless) experiments

--- routes/order.js
+++ routes/order.js
@@ -13,7 +13,8 @@ objects below
 
 Note about special types:
 - "PK" = primary key, not displayed
-- "FK" = foreign key, creates a dropdown list linked to the ressource after ':'
+- "FK" = foreign key, creates a dropdown list for resource "data", 
+  filtered by resource "parent"
 - "currency": stored as an integer but displayed with 2 decimals
 */
 
@@ -23,15 +24,17 @@ const formFields = [
     displayLabel: "hidden",
     displayOrder: -1,
     type: "PK",
-    filteredBy: "",
+    data: "",
+    parent: "",
     mandatory: true
   },
   {
     dbFieldName: "shopid",
     displayLabel: "Shop",
     displayOrder: 1,
-    type: "FK:shops",
-    filteredBy: "",
+    type: "FK",
+    data: "shops",
+    parent: "",
     mandatory: true
   },
   {
@@ -39,7 +42,8 @@ const formFields = [
     displayLabel: "Date",
     displayOrder: 2,
     type: "date",
-    filteredBy: "",
+    data: "",
+    parent: "",
     mandatory: true
   },
   {
@@ -47,7 +51,8 @@ const formFields = [
     displayLabel: "Order number",
     displayOrder: 3,
     type: "text",
-    filteredBy: "",
+    data: "",
+    parent: "",
     mandatory: true
   },
   {
@@ -55,7 +60,8 @@ const formFields = [
     displayLabel: "Tracking number",
     displayOrder: 4,
     type: "text",
-    filteredBy: "",
+    data: "",
+    parent: "",
     mandatory: false
   },
   {
@@ -63,7 +69,8 @@ const formFields = [
     displayLabel: "Amount in ฿",
     displayOrder: 5,
     type: "integer",
-    filteredBy: "",
+    data: "",
+    parent: "",
     mandatory: true
   },
   {
@@ -71,7 +78,8 @@ const formFields = [
     displayLabel: "Amount in $",
     displayOrder: 6,
     type: "currency",
-    filteredBy: "",
+    data: "",
+    parent: "",
     mandatory: false
   },
   {
@@ -79,7 +87,8 @@ const formFields = [
     displayLabel: "Shipping cost in ฿",
     displayOrder: 7,
     type: "integer",
-    filteredBy: "",
+    data: "",
+    parent: "",
     mandatory: false
   },
   {
@@ -87,28 +96,118 @@ const formFields = [
     displayLabel: "Shipping cost in $",
     displayOrder: 8,
     type: "currency",
-    filteredBy: "",
+    data: "",
+    parent: "",
     mandatory: false
   }
 ];
 
-const mapValuesWithFieldDefinitions = result => item => ({
-  ...item,
-  value: result.rows[0][item.dbFieldName]
-});
+const mapValuesWithFieldDefinitions = row => item => {
+  if (item.type === "url") {
+    const urlField = row[item.data];
+    console.log(urlField);
+    return {
+      ...item,
+      data: row[urlField],
+      value: row[item.dbFieldName]
+    };
+  } else {
+    return {
+      ...item,
+      value: row[item.dbFieldName]
+    };
+  }
+};
 
 const getOrder = (req, res) =>
   db
     .query(SQL_QUERY_GET_ORDER, [req.params["orderId"]])
-    .then(result => formFields.map(mapValuesWithFieldDefinitions(result)))
+    .then(result =>
+      formFields.map(mapValuesWithFieldDefinitions(result.rows[0]))
+    )
     .then(data => res.status(200).send(data))
     .catch(e => {
       console.log(e.stack);
       res.status(500).send(e);
     });
 
+const orderListFields = [
+  {
+    dbFieldName: "orderid",
+    displayLabel: "hidden",
+    displayOrder: -1,
+    type: "PK",
+    data: "",
+    parent: "",
+    mandatory: true
+  },
+  {
+    dbFieldName: "date",
+    displayLabel: "Date",
+    displayOrder: 1,
+    type: "date",
+    data: "",
+    parent: "",
+    mandatory: true
+  },
+  {
+    dbFieldName: "shopname",
+    displayLabel: "Shop",
+    displayOrder: 2,
+    type: "url",
+    data: "shopurl",
+    parent: "",
+    mandatory: true
+  },
+  {
+    dbFieldName: "totalamountinbaht",
+    displayLabel: "Amount in ฿",
+    displayOrder: 5,
+    type: "integer",
+    data: "",
+    parent: "",
+    mandatory: true
+  },
+  {
+    dbFieldName: "ordernumber",
+    displayLabel: "Order number",
+    displayOrder: 3,
+    type: "text",
+    data: "",
+    parent: "",
+    mandatory: false
+  },
+  {
+    dbFieldName: "trackingnumber",
+    displayLabel: "Tracking number",
+    displayOrder: 4,
+    type: "text",
+    data: "",
+    parent: "",
+    mandatory: false
+  },
+  {
+    dbFieldName: "totalamountinusdcents",
+    displayLabel: "Amount in $",
+    displayOrder: 6,
+    type: "currency",
+    data: "",
+    parent: "",
+    mandatory: false
+  },
+  {
+    dbFieldName: "shippingcostinusdcents",
+    displayLabel: "Shipping cost in $",
+    displayOrder: 8,
+    type: "currency",
+    data: "",
+    parent: "",
+    mandatory: false
+  }
+];
+
 const SQL_QUERY_MANAGE_ORDERS = `
-SELECT T.TeaId, T.Name as TeaName, S.Name as ShopName, S.url, 
+SELECT T.TeaId, T.Name as TeaName, S.Name as ShopName, S.url as ShopUrl, 
 O.OrderId, O.OrderNumber, O.TrackingNumber, 
 to_char(O.Date, 'DD/MM/YYYY') as OrderDate, O.TotalAmountInBaht, 
 O.ShippingCostInBaht, O.TotalAmountInUsdCents, O.ShippingCostInUsdCents 
@@ -116,6 +215,10 @@ O.ShippingCostInBaht, O.TotalAmountInUsdCents, O.ShippingCostInUsdCents
 FROM "order" O join Shop S on S.ShopId=O.ShopId 
 left join OrderTea OT on O.OrderId=OT.OrderId 
 left join Tea T on OT.TeaId=T.TeaId`;
+
+const fillData = orderData => {
+  const fields = formFields.map(mapValuesWithFieldDefinitions(orderData));
+};
 
 // Unflatten the result: array of Orders, each Order contains a list of Teas
 const groupTeasByOrder = (orderList, row) => {
