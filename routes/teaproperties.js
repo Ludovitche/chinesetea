221724bac7ab549d14f3f44daf11a64625068851
Ludovitche <Ludovitche@users.noreply.github.com>
2019-08-10 21:24:05 +0700

    1 more bug fix

--- routes/teaproperties.js
+++ routes/teaproperties.js
@@ -9,17 +9,20 @@ const SQL_QUERY_GET_LOCATIONS = `SELECT LocationId, Name FROM Location`;
 const getAllTeaDropdownLists = (req, res) => {
   let result = {};
   return Promise.all([
-    db
-      .simpleQuery(SQL_QUERY_GET_FORMATS)
-      .then(data => (result["formats"] = data.rows)),
-    db
-      .simpleQuery(SQL_QUERY_GET_ROLES)
-      .then(data => (result["currentRoles"] = data.rows)),
-    db
-      .simpleQuery(SQL_QUERY_GET_LOCATIONS)
-      .then(data => (result["locations"] = data.rows))
+    db.simpleQuery(SQL_QUERY_GET_FORMATS).then(data => {
+      result["formats"] = data.rows;
+      return true;
+    }),
+    db.simpleQuery(SQL_QUERY_GET_ROLES).then(data => {
+      result["currentRoles"] = data.rows;
+      return true;
+    }),
+    db.simpleQuery(SQL_QUERY_GET_LOCATIONS).then(data => {
+      result["locations"] = data.rows;
+      return true;
+    })
   ])
-    .then(result => res.status(200).send(result))
+    .then(data => res.status(200).send(result))
     .catch(e => {
       console.log(e.stack);
       res.status(500).send(e);
