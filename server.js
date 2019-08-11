8b5066fed5eb6796a0229c11a85caf0c2d6df282
Ludovitche <Ludovitche@users.noreply.github.com>
2019-08-11 23:39:46 +0700

    tea get queries step 1 - 2

--- server.js
+++ server.js
@@ -3,19 +3,23 @@
 const express = require("express");
 const app = express();
 
-const { getAllTeaDropdownLists } = require("./routes/options");
+// Main objects are Order and Tea - an Order is linked to 0 or many teas
+// All the other ressources are tea properties
 
 const {
   getAllOrdersAndTeas,
   getOrderById,
   getOrderFields
 } = require("./routes/order");
+
 const {
   getTeasWithFilters,
   getTeaById,
   getTeaFilters,
   getTeaFields
 } = require("./routes/tea");
+
+const { getAllTeaDropdownLists } = require("./routes/options");
 
 const { getAllCountries } = require("./routes/country");
 const { getAllAreasWithCountryName } = require("./routes/area");
@@ -24,39 +28,31 @@ const { getAllSubTypesWithTypeName } = require("./routes/subtype");
 const { getAllShops } = require("./routes/shop");
 
 // when creating a new 'order' or 'tea' resource, the client will create form
-// fields dynamically, using the list of fields returned by below requests
-// I didn't find a 'best practice' uri format for this
-// For tea, we will also create filters dynamically
+// fields dynamically, using the list of fields returned by the requests below
 app.get("/orders/fields", getOrderFields);
 app.get("/teas/fields", getTeaFields);
 app.get("/teas/filters/fields", getTeaFilters);
 
-// the rest however is a 'normal' REST API (but no hateoas)
 app.get("/orders", getAllOrdersAndTeas);
 app.get("/orders/:orderId", getOrderById);
-// the request below will accept a lof of filters in format :
-// teas?filter1=value1&filter2=value2
+// the request below will accept filters, see teaFilterFields.js for details
 app.get("/teas", getTeasWithFilters);
-// we get the data for all dropdown list in 1 request (it will remain small)
+// we get all dropdown list values in 1 request
 app.get("/teas/options", getAllTeaDropdownLists);
 app.get("/teas/:teaId", getTeaById);
 
-// These resources are the tea options that can be modified (not all)
-// These requests, including the GET, are meant to be accessed only in settings
-// Each result of get query contains a calculated field indicating if the
-// resource can be deleted or not
+// These requests, including the GET, should be used only in screen Settings
+// Get queries return a calculated field allowing or not to delete the resource
 app.get("/countries", getAllCountries);
 app.get("/areas", getAllAreasWithCountryName);
 app.get("/types", getAllTypes);
 app.get("/subTypes", getAllSubTypesWithTypeName);
 app.get("/shops", getAllShops);
 
-// This needs to be last (default routing)
 app.use(function(req, res) {
   res.status(404).send({
     url: req.originalUrl + " not found"
   });
 });
 
-// Port 5000 is the default Dokku application port
 app.listen(5000, () => console.log("Listening on port 5000"));
