b3afc40b26b4bf856d3532808e192e29c34e991f
Ludovitche <Ludovitche@users.noreply.github.com>
2019-08-25 18:01:22 +0700

    Implement delete queries - bug fix 11

--- routes/order.js
+++ routes/order.js
@@ -147,13 +147,9 @@ RETURNING TeaId
 
 const createTeasDeleteQuery = (queryStartText, queryEndText, parameters) => {
   const query =
-    queryStartText +
-    parameters
-      .map((param, index) => "$" + (index + 1))
-      .join(", ")
-      .reduce((acc, item) => acc + item, "") +
-    queryEndText;
-  console.log("Where in query created: query");
+    queryStartText + parameters.map((param, index) => "$" + (index + 1)).join();
+  queryEndText;
+  console.log("Where in query created: " + query);
   return query;
 };
 
