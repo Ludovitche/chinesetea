e7dbbc7a80c19c7ed9cbad3e383c435c23cb4707
Ludovitche <ludovitche@gmail.com>
2019-09-04 12:28:40 +0700

    add update tea+ordertea methods - bug fix 1

--- routes/tea.js
+++ routes/tea.js
@@ -419,7 +419,7 @@ const updateTeaAndLastOrderTea = (req, res) => {
     res.status(400).send({ Status: 400, Error: "Missing URI parameter" });
   } else {
     const teaId = req.params["teaId"];
-    return queries
+    queries
       .getQueryRoute(SQL_QUERY_FIND_LAST_ORDER_FOR_TEA, [teaId])
       .then(result => {
         if (result.rowCount > 0) {
